c96cdfd682a6d53adb342f743df5659336a52c08
Marco <brandt.marco05@gmail.com>
2025-07-06 22:20:49 +0200

    removed tests. Write new ones

--- src/test/java/com/observe/os1/systemDataControllers/SystemInfoModelControllerTest.java
+++ src/test/java/com/observe/os1/systemDataControllers/SystemInfoModelControllerTest.java
@@ -13,40 +13,5 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @QuarkusTest
 public class SystemInfoModelControllerTest {
 
-    @Inject
-    SystemInfoController controller;
 
-    @BeforeEach
-    @Transactional
-    public void clearDb() {
-        SystemInfoModel.deleteAll();
-    }
-
-
-    // Test if SystemInfo entry is created on startup
-    @Test
-    @Transactional
-    public void testSystemInfoEntryExists() {
-        controller.onStartup(null); // simulate startup event
-
-        SystemInfoModel infoObj = SystemInfoModel.findAll().firstResult();
-
-        assertNotNull(infoObj, "SystemInfo should have been created on startup");
-        assertNotNull(infoObj.startDateTime, "Start time should be set");
-
-        // TODO temporary user should be created on first startup
-
-    }
-
-    // Test 3 startup events
-    @Test
-    @Transactional
-    public void testThreeStartupSimulations() {
-        controller.onStartup(null); // simulate first startup
-        controller.onStartup(null); // simulate second startup
-        controller.onStartup(null); // simulate third startup
-
-        long count = SystemInfoModel.count();
-        assertEquals(3, count, "Should have 3 startup records");
-    }
 }
--- src/test/java/com/observe/os1/v1/connect/NewClientResourceTest.java
+++ src/test/java/com/observe/os1/v1/connect/NewClientResourceTest.java
@@ -11,27 +11,5 @@ import static org.hamcrest.Matchers.matchesRegex;
 @QuarkusTest
 public class NewClientResourceTest {
 
-    // TODO adopt the tests so they use an admin API key
-    @Test
-    public void testSuccessfulRequestFromLocalhost() {
-        RestAssured.given()
-                .header("X-Forwarded-For", "127.0.0.1")
-                .when()
-                .post("/v1/new-connection")
-                .then()
-                .statusCode(Response.Status.OK.getStatusCode())
-                .body(matchesRegex("^[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}$"));
-    }
-
-    @Test
-    public void testRequestFromUnknownSourceIsForbidden() {
-        RestAssured.given()
-                .header("X-Forwarded-For", "8.8.8.8")
-                .when()
-                .post("/v1/new-connection")
-                .then()
-                .statusCode(Response.Status.FORBIDDEN.getStatusCode())
-                .body(Matchers.equalTo("Access denied. Just for local access."));
-    }
 
 }
