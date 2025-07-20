b2fc58ca9e1f5c43c8157ccd32110ef0dccb7cf2
Marco <brandt.marco05@gmail.com>
2025-07-20 02:16:08 +0200

    let test work

---
+++ src/test/java/com/observe/os1/v1/HealthCheckTest.java
@@ -0,0 +1,20 @@
+package com.observe.os1.v1;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class HealthCheckTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+            .when().get("/health-check")
+            .then()
+            .statusCode(200)
+            .body(is("Hello from Quarkus!"));
+    }
+}
