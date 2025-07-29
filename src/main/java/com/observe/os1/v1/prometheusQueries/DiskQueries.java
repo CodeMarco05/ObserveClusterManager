d83682fce4b8cca58d49337889cd0936b2131ce0
Marco <brandt.marco05@gmail.com>
2025-07-29 16:31:35 +0200

    added the disk endpoints

--- src/main/java/com/observe/os1/v1/prometheusQueries/DiskQueries.java
+++ src/main/java/com/observe/os1/v1/prometheusQueries/DiskQueries.java
@@ -1,7 +1,14 @@
 package com.observe.os1.v1.prometheusQueries;
 
 public enum DiskQueries {
-    DISK_USAGE_IN_GB("(1 - (node_filesystem_avail_bytes / node_filesystem_size_bytes)) * 100");
+    DISK_AVAILABLE_IN_GB("sum(node_filesystem_avail_bytes{fstype!~\"tmpfs|overlay\"}) / 1073741824\n"),
+    DISK_USAGE_IN_GB("(\n" +
+            "  sum(node_filesystem_size_bytes{fstype!~\"tmpfs|overlay\"}) \n" +
+            "  - \n" +
+            "  sum(node_filesystem_avail_bytes{fstype!~\"tmpfs|overlay\"})\n" +
+            ") / 1073741824\n"),
+    DISK_TOTAL_SIZE_IN_GB_ALL_SYSTEMS("sum(node_filesystem_size_bytes{fstype!~\"tmpfs|overlay\"}) / 1073741824\n"),
+    DISK_TOTAL_SIZE_IN_GB("node_filesystem_size_bytes{mountpoint=\"/\", fstype!~\"tmpfs|overlay\"} / 1073741824\n");
 
     private final String query;
 
