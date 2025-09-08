1161d56dadc1e58835a7179fb286f9bd87588ab7
CodeMarco05 <brandt.marco05@gmail.com>
2025-09-08 23:20:27 +0200

    keycloak test

--- db-init-scripts/init.dev.sql
+++ db-init-scripts/init.dev.sql
@@ -2,13 +2,19 @@
 CREATE DATABASE observe_db;
 CREATE DATABASE observe_db_test;
 
+CREATE DATABASE keycloak;
+
 -- Create users
 CREATE USER observer WITH ENCRYPTED PASSWORD 'theOverlookingEagle';
 CREATE USER observer_test WITH ENCRYPTED PASSWORD 'theOverlookingEagle_test';
 
+CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloak';
+
 -- Grant privileges to databases
 GRANT CONNECT ON DATABASE observe_db TO observer;
 GRANT CONNECT ON DATABASE observe_db_test TO observer_test;
+
+GRANT CONNECT ON DATABASE keycloak TO keycloak;
 
 -- Work inside observe_db
 \c observe_db
@@ -19,3 +25,8 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO observer;
 \c observe_db_test
 GRANT USAGE, CREATE ON SCHEMA public TO observer_test;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO observer_test;
+
+-- Work inside keycloak
+\c keycloak
+GRANT USAGE, CREATE ON SCHEMA public TO keycloak;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO keycloak;
