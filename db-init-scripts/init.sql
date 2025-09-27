77cb54d1de0875f671be9a19e69ed60cf551fdc5
Marco <brandt.marco05@gmail.com>
2025-09-27 16:58:11 +0200

    Removes obsolete configuration files
    This commit removes several configuration files and scripts that are no longer needed.
    These files include application.yml for both main and test environments, database initialization scripts, and a CPU information script.
    These files are considered obsolete because the application architecture and deployment strategies have changed.

--- db-init-scripts/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE observe_db;
-
-CREATE USER observer WITH ENCRYPTED PASSWORD 'theOverlookingEagle';
-
-\c observe_db
-GRANT CONNECT ON DATABASE observe_db TO observer;
-
-GRANT USAGE, CREATE ON SCHEMA public TO observer;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO observer;
