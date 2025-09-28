c215c31e76abf2d299d5f24a65275401ef88b876
CodeMarco05 <brandt.marco05@gmail.com>
2025-09-28 11:01:10 +0200

    Added prometheus queries

--- src/main/java/org/observe/rest/client/Queries.java
+++ src/main/java/org/observe/rest/client/Queries.java
@@ -15,6 +15,12 @@ public enum Queries {
     NETWORK_IN_IN_BYTES_PER_SECOND("sum(rate(node_network_receive_bytes_total{device!~\"lo\"}[10s]))"),
     NETWORK_OUT_IN_BYTES_PER_SECOND("sum(rate(node_network_transmit_bytes_total{device!~\"lo\"}[10s]))"),
 
+    // Queries for Disk usage
+    TOTAL_DISK_SIZE_IN_GB_FOR_ALL_SYSTEMS_SUMMED_UP("sum(node_filesystem_size_bytes{fstype!~\"tmpfs|overlay\"}) / 1024^3"),
+    TOTAL_DISK_SIZE_IN_GB_FOR_ALL_SYSTEMS("node_filesystem_size_bytes{mountpoint=\"/\", fstype!~\"tmpfs|overlay\"} / 1024^3"),
+    TOTAL_DISK_USAGE_IN_GB_FOR_ALL_SYSTEMS_SUMMED_UP("sum(node_filesystem_size_bytes{fstype!=\"tmpfs\"} - node_filesystem_avail_bytes{fstype!=\"tmpfs\"}) / 1024^3"),
+
+
     // general queries
     UPTIME_IN_SECONDS("(node_time_seconds - node_boot_time_seconds)"),
     UP_QUERY("up");
